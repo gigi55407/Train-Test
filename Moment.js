feda88c69859b38ac47613ff796c133987eca851
gigi55407 <46692649+gigi55407@users.noreply.github.com>
2019-04-04 16:54:54 -0500

    sdfvg

---
+++ Moment.js
@@ -0,0 +1,14 @@
+<script>
+
+<script src="https://code.jquery.com/jquery.js"></script>
+
+<script src="https://cdn.jsdelivr.net/momentjs/2.12.0/moment.min.js"></script>
+
+
+var todayDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+console.log(todayDate);
+
+
+
+</script>
